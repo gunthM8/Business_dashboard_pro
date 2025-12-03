55958250d5b746bb255d2a9b345002af1cda96d0
Gunther Hayes <guntheruhayes@gmail.com>
2025-12-02 19:23:26 -0700

    Uncommented LOGIN middleware

--- server.js
+++ server.js
@@ -63,12 +63,12 @@ pool.getConnection((err, connection) => {
 });
 
 // LOGIN REQUIRED middleware
-//function requireLogin(req, res, next) {
-  //if (!req.session.userId) {
-    //return res.status(401).json({ error: "Not authenticated" });
-  //}
-  //next();
-//}
+function requireLogin(req, res, next) {
+  if (!req.session.userId) {
+    return res.status(401).json({ error: "Not authenticated" });
+  }
+  next();
+}
 
 // AUTH ROUTES
 app.post("/api/register", async (req, res) => {
